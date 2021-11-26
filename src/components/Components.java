782ec0c0bda72f4eeb919e4c2d18c32770f0ed1f
Erwan <erwan.beving@gmail.com>
2021-11-26 10:26:06 +0100

    Reworked entity and components

--- src/components/Components.java
+++ src/components/Components.java
@@ -1,5 +1,34 @@
 package components;
 
+import entities.Entity;
+import main.Handler;
+
 public abstract class Components {
+
+	private Entity entity;
+
+	public final void setEntity(Entity e) {
+		entity = e;
+	}
+
+	public final Entity getEntity() {
+		return entity;
+	}
+
+	final protected <T> T getSystem(Class<T> clazz) throws IllegalArgumentException,
+	NullPointerException {
+		return entity.getHandler().getSystem(clazz);
+	}
+	
+	final protected <T> T getComponent(Class<T> clazz)
+            throws IllegalArgumentException, NullPointerException {
+        return entity.getComponent(clazz);
+    }
+	
+	final protected Handler getHandler() throws NullPointerException {
+        return entity.getHandler();
+    }
+	
+	
 
 }
--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -2,52 +2,86 @@ package entities;
 
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import components.Components;
 import main.Handler;
 
 public class Entity {
-	
+
 	protected Handler handler;
-	protected Set<Components> components = new HashSet<Components>();
+	private List<Components> components = new ArrayList<>();
+	private Map<Class<?>, Components> cache = new HashMap<>();
 
 	public void render(Graphics g) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void tick() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	public void onMouseMove(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+	public boolean hasComponent(Class<?> clazz) {
+		if (cache.containsKey(clazz)) {
+			return true;
+		}
+
+		for (Components c : components) {
+			if (clazz.isInstance(c)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
-	public void onMouseRelease(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+	public <T> T getComponent(Class<T> clazz) throws IllegalArgumentException {
+		Components cached = cache.get(clazz);
+		if (cached != null) {
+			return clazz.cast(cached);
+		}
+
+		for (Components c : components) {
+			if (clazz.isInstance(c)) {
+				cache.put(clazz, c);
+				return clazz.cast(c);
+			}
+		}
+		throw new IllegalArgumentException("component not found "
+				+ clazz.getName());
 	}
 
-	public Set<Components> getComponents() {
-		return components;
+	public <T> List<T> getAllComponents(Class<T> clazz) {
+		ArrayList<T> result = new ArrayList<T>();
+		for (Components c : components) {
+			if (clazz.isInstance(c)) {
+				result.add(clazz.cast(c));
+			}
+		}
+
+		return result;
 	}
 
-	public void setComponents(Set<Components> components) {
-		this.components = components;
+	public void addComponent(Components c) {
+		components.add(c);
+		c.setEntity(this);
+	}
+
+	public Handler getHandler() {
+		return handler;
+	}
+
+	public void setHandler(Handler handler) {
+		this.handler = handler;
 	}
 	
-	public void addComponents(Components c) {
-		
-		components.add(c);
-		components.
-		
-	}
+	
 	
 
 }
